e4fc69399dd78d918a807832755eef060ee566e2
solvely <p.solvely@gmail.com>
2016-12-25 23:21:21 +0900

    [new] 프로젝트 생성

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by sol on 2016. 12. 25..
+//  Copyright © 2016년 solsol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
